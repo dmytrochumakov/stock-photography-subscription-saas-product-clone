2b74977f9cbdfa2079b22b98d12d492dfdc77beb
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2024-08-23 13:30:18 +0300

    added checkout route

--- stripe-hono/src/index.ts
+++ stripe-hono/src/index.ts
@@ -3,6 +3,7 @@ import { Hono } from 'hono'
 import "dotenv/config";
 
 import Stripe from 'stripe';
+import { HTTPException } from 'hono/http-exception';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 
@@ -11,6 +12,39 @@ const app = new Hono()
 app.get('/', (c) => {
   return c.text('Hello world!')
 })
+
+app.post('/checkout', async (c) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price: "price_1PqubGDI2hkw3CnRL5o9kQup",
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'http://localhost:3000/success',
+      cancel_url: 'http://localhost:3000/cancel',
+    });
+    return c.json(session);
+  } catch (error: any) {
+    console.log(error);
+    throw new HTTPException(500, { message: error?.message });
+  }
+});
+
+app.get('/success', (c) => {
+  return c.text('Payment successful')
+});
+
+
+app.get('/cancel', (c) => {
+  return c.text('Payment cancelled')
+});
+
+
+
 
 const port = 3000
 console.log(`Server is running on port ${port}`)
