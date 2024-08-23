276e7dd4c0a11568b3d713417c60c675211820dd
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2024-08-23 13:56:55 +0300

    added webhook

--- stripe-hono/src/index.ts
+++ stripe-hono/src/index.ts
@@ -63,6 +63,37 @@ app.post('/checkout', async (c) => {
   }
 });
 
+app.post('/webhook', async (c) => {
+  const rawBody = await c.req.text();
+  const signature = c.req.header('stripe-signature');
+
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(rawBody, signature!, process.env.STRIPE_WEBHOOK_SECRET!);
+  } catch (error: any) {
+    console.log('webhook signature verification failed: ${error.message}');
+    throw new HTTPException(400)
+  }
+
+  if (event.type === 'checkout.session.completed') {
+    const session = event.data.object;
+    console.log('checkout session completed', session);
+  }
+
+  if (event.type === 'customer.subscription.updated') {
+    const subscription = event.data.object;
+    console.log(subscription);
+  }
+
+  if (event.type === 'customer.subscription.deleted') {
+    const subscription = event.data.object;
+    console.log(subscription);
+  }
+
+
+  return c.text('success');
+});
+
 app.get('/success', (c) => {
   return c.text('Payment successful')
 });
